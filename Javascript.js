cdb87c90e0e951af216ad7a6ff7683bce65d3b9f
PuffbuttJP <65969209+PuffbuttJP@users.noreply.github.com>
2022-08-20 15:39:09 +0100

    Add files via upload

---
+++ Javascript.js
@@ -0,0 +1,34 @@
+console.log('Hello World');
+
+const Zero = document.querySelector('#filler');
+const One = document.querySelector('#prof');
+const Two = document.querySelector('#acad');
+const Three = document.querySelector('#pers');
+ 
+function showFill() {
+    Zero.style.display="flex";
+    One.style.display="none";
+    Two.style.display="none";
+    Three.style.display="none";
+}
+
+function showProf() {
+    Zero.style.display="none";
+    One.style.display="flex";
+    Two.style.display="none";
+    Three.style.display="none";
+}
+
+function showAcad() {
+    Zero.style.display="none";
+    One.style.display="none";
+    Two.style.display="flex";
+    Three.style.display="none";
+}
+
+function showPers() {
+    Zero.style.display="none";
+    One.style.display="none";
+    Two.style.display="none";
+    Three.style.display="flex";
+}
